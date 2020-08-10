fe47d07c4f4abfe84243b398c33b96b31d843905
Eddy <bishclapper69420@gmail.com>
2020-08-10 13:14:20 +0300

    added verbosity

--- smp.c
+++ smp.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include "config.h"
 int main()
 {
+	bool verbose = false;
 	char c;
 	printf("-Simple MPD Player-\n");
 
@@ -22,24 +24,47 @@ int main()
 			break;
 		case 'h':
 			printf(cprompt);
-			printf("Help Page:\n \t welcome to the help page! heres a list of the available commands: \n\t + = increases the volume. instructing more than one + increases it by the amount of + specified \n\t - = same thing as +, but decreases the volume. \n\t <spacebar> = pause/unpause the music \n\t n = next song \n\t p = previous song \n\t s <args> = searches and plays the song \n\t CTRL+L also clears the chat. \n\t t = list the song thats being played\n\t f to fast forward, b to fast backward  \n\n edit keybinds: \n\t to edit the keybinds edit your config.h file.\n");
+			printf("Help Page:\n \t welcome to the help page! heres a list of the available commands: \n\t + = increases the volume. instructing more than one + increases it by the amount of + specified \n\t - = same thing as +, but decreases the volume. \n\t <spacebar> = pause/unpause the music \n\t n = next song \n\t p = previous song \n\t s <args> = searches and plays the song \n\t CTRL+L also clears the chat. \n\t t = list the song thats being played\n\t f to fast forward, b to fast backward \n\t v/V = enable/disable verbosity \n\n edit keybinds: \n\t to edit the keybinds edit your config.h file.\n");
 			break;
+		case 'v':
+			verbose = true;
+			printf("verbose = on\n");
+			printf(cprompt);
+			break;
+		case 'V':
+			verbose = false;
+			printf("verbose = off\n");
+			printf(cprompt);
+			break;
+		case 'q':
+			exit(1);
+		case 'Q':
+			exit(1);
 		}
 
-		if(c == skipsong)
+		if(c == skipsong && verbose == false)
 		{
 			printf(cprompt);
 			system("mpc -q next");
+		}else if(c == skipsong){
+			printf(cprompt);
+			system("mpc next");
 		}
-		if (c == prevsong)
+		if (c == prevsong && verbose == false)
 		{
 			printf(cprompt);
 			system("mpc -q prev");
+		}else if(c == prevsong){
+			printf(cprompt);
+			system("mpc next");
 		}
-		if (c == toggle)
+		if (c == toggle && verbose == false)
 		{
 			printf(cprompt);
 			system("mpc -q toggle");
+		}else if(c == toggle){
+			printf(cprompt);
+			system("mpc toggle");
 		}
 		if (c == clearconsole)
 		{
