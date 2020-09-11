0fc18ad4430e340ca09983555771f136637eb1a9
Eddy-Arch <bishclapper69420@gmail.com>
2020-09-11 08:29:22 +0300

    added a backup config

---
+++ src/config.def.h
@@ -0,0 +1,31 @@
+/*
+ * a backup of the default config, for your convenience.
+*/
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+//change increase/decrease interval
+char *upinterval = "10";
+char *downinterval = "10";
+//change the seek interval
+char *seekforwardint = "10";
+char *seekbackint = "10";
+
+//keybinds//
+const char skipsong = 'n';
+const char prevsong = 'p';
+const char toggle = ' ';
+const char clearconsole = 'c';
+const char search_play = 's';
+const char nowplaying = 't';
+const char fastforward = 'f';
+const char fastbackward = 'b';
+const char list_title = 'l';
+const char play_pos = 'z';
+const char list_playlist = 'd';
+const char enable_raw = 'r';
+const char enable_verbose = 'v';
+const char enable_headfull = 'H';
+//change the prompt
+char cprompt[] = "\nSMP console ~$ ";
+
